208f503440d658bb5edfadf8bd94020d011cf0c0
Gus Silva <gus7silva@yahoo.com>
2015-04-07 11:47:59 -0700

    Initial commit

---
+++ app/src/main/java/com/example/gussilva/threemorewords/GameActivity.java
@@ -0,0 +1,107 @@
+package com.example.gussilva.threemorewords;
+
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Random;
+
+
+public class GameActivity extends ActionBarActivity {
+
+    private String[] words;
+    private Random rand;
+    private String currWord;
+    private LinearLayout wordLayout;
+    private TextView[] charViews;
+    private GridView letters;
+    private LetterAdapter ltrAdapt;
+
+    //body part images
+    private ImageView[] bodyParts;
+    //number of body parts
+    private int numParts=6;
+    //current part - will increment when wrong answers are chosen
+    private int currPart;
+    //number of characters in current word
+    private int numChars;
+    //number correctly guessed
+    private int numCorr;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        Resources res = getResources();
+        words = res.getStringArray(R.array.words);
+
+        rand = new Random();
+        currWord = "";
+
+        wordLayout = (LinearLayout)findViewById(R.id.word);
+        letters = (GridView)findViewById(R.id.letters);
+
+        playGame();
+    }
+
+    private void playGame() {
+
+        //play a new game
+        String newWord = words[rand.nextInt(words.length)];
+        while(newWord.equals(currWord)) newWord = words[rand.nextInt(words.length)];
+        currWord = newWord;
+        charViews = new TextView[currWord.length()];
+        wordLayout.removeAllViews();
+        wordLayout.removeAllViews();
+
+        //Set TextViews text to letter of the answer
+        for (int c = 0; c < currWord.length(); c++) {
+            charViews[c] = new TextView(this);
+            charViews[c].setText(""+currWord.charAt(c));
+
+            //Set TextView Properties
+            charViews[c].setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            charViews[c].setGravity(Gravity.CENTER);
+            charViews[c].setTextColor(Color.TRANSPARENT); //So that user does not see that answer
+            charViews[c].setBackgroundResource(R.drawable.letter_bg);
+            //add to layout
+            wordLayout.addView(charViews[c]);
+        }
+
+        ltrAdapt=new LetterAdapter(this);
+        letters.setAdapter(ltrAdapt);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_game, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
